ad2b1ce28e98bb75a7cc370041610b70b49e149f
Brothin Das <78947331+Brothin@users.noreply.github.com>
2022-08-19 23:58:17 +0530

    Update global_variables.dart

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-String uri = 'http://192.168.42.179:3000';
+String uri = 'https://ecommerce-app-brothin.herokuapp.com/';
 
 class GlobalVariables {
   // COLORS
